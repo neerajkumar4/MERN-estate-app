41bfb6b0a60dab4701637d0e47e2e33bd745439b
Neeraj Kumar <nk4976744@gmail.com>
2024-01-03 19:26:23 +0530

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, Signin, Signup, About, Profile } from "./pages";
 
 const App = () => {
-  return <div className="App">Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+export {default as Home} from "./Home"
+export {default as Signin} from "./Signin"
+export {default as Signup} from "./Signup"
+export {default as Profile} from "./Profile"
+export {default as About} from "./About"
