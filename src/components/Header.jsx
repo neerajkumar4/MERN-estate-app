4afada26bea69d4d79da3663cb090120acb2c082
Neeraj Kumar <nk4976744@gmail.com>
2024-01-08 22:08:14 +0530

    responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,48 @@
-import React from "react";
-import {FaSearch} from "react-icons/fa";
+import React, { useState } from "react";
+import { IconButton } from "rsuite";
+import { Search } from "@rsuite/icons";
+import ArowBackIcon from "@rsuite/icons/ArowBack";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [search, setSearch] = useState(false);
   return (
     <header className="header">
-      <div className="navbar">
-        <h1 className="logoTxt">
-          <span className="logo1">Makaan</span>
-          <span className="logo2">Estate</span>
-        </h1>
+      {search ? (
         <form className="searchBar">
-            <input type="text" placeholder="Search..." />
-            <FaSearch size={30} />
+          <IconButton
+            className="hideSearchIcon"
+            icon={<ArowBackIcon />}
+            onClick={() => setSearch(!search)}
+          />
+          <input type="text" placeholder="Search..." />
         </form>
-        <ul className="navbarList">
-          <li>Home</li>
-          <li>About</li>
-          <li>Sign in</li>
-        </ul>
-      </div>
+      ) : (
+        <div className="navbar">
+          <h1 className="logoTxt">
+            <span className="logo1">Mak</span>
+            <span className="logo2">Estate</span>
+          </h1>
+          <div className="navTabs">
+            <IconButton
+              className="searchIcon"
+              icon={<Search />}
+              onClick={() => setSearch(!search)}
+            />
+            <ul className="navbarList">
+              <Link className="Link homeTab" to="/">
+                <li>Home</li>
+              </Link>
+              <Link className="Link aboutTab" to="/about">
+                <li>About</li>
+              </Link>
+              <Link className="Link" to="/sign-in">
+                <li>SignIn</li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
