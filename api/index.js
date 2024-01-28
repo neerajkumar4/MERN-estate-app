3c90d489c52c6829cc2e3cb2becf9bc5653cd09a
Neeraj Kumar <nk4976744@gmail.com>
2024-01-28 22:33:59 +0530

    signup page added

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -13,10 +15,11 @@ mongoose
     console.log(err);
   });
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api/test", userRouter);
 app.use("/api/auth", authRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running at port 3000!");
+app.listen(4000, () => {
+  console.log("Server is running at port 4000!");
 });
