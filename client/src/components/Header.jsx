429761b7fcf07616e0277ae37b5f8c42d21dad47
Neeraj Kumar <nk4976744@gmail.com>
2024-02-08 22:59:01 +0530

    search header functinality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,23 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IconButton } from "rsuite";
 import { Search } from "@rsuite/icons";
 import ArowBackIcon from "@rsuite/icons/ArowBack";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Header = () => {
   const [search, setSearch] = useState(false);
-  const {currentUser} = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const query = urlParams.toString();
+    navigate(`search?${query}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [window.location.search]);
   return (
     <header className="header">
       {search ? (
-        <form className="searchBar">
+        <form className="searchBar" onSubmit={handleSearch}>
           <IconButton
             className="hideSearchIcon"
             icon={<ArowBackIcon />}
             onClick={() => setSearch(!search)}
           />
-          <input type="text" placeholder="Search..." />
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
         </form>
       ) : (
         <div className="navbar">
