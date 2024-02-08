d28a2ee49ac16e1c246c86564f59cfd36a1d1804
Neeraj Kumar <nk4976744@gmail.com>
2024-02-08 19:05:44 +0530

    search api

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,6 +3,7 @@ import {
   createListing,
   deleteListing,
   getListing,
+  getListings,
   updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
@@ -14,6 +15,6 @@ const router = express.Router();
 router.post("/create", createListing);
 router.delete("/delete/:id", deleteListing);
 router.post("/update/:id", updateListing);
-router.get('/get/:id', getListing);
+router.get("/get/:id", getListing);
+router.get("/get", getListings);
 export default router;
-
